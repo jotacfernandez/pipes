50437a5c5c0820c37b0c7fb9b71d22a3a7a68f7d
jc.fernandez <jcfdezrguez@gmail.com>
2022-04-26 22:19:08 +0200

    NavBar2 y Logo brand

--- wikifilms/src/app/components/components.module.ts
+++ wikifilms/src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule //importamos este módulo para poder hacer uso de routerlink en el componente navbar, entre otros
   ]
 })
 export class ComponentsModule { }
