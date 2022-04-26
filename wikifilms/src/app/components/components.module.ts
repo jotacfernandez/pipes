749ad0abbc8ab1018b6f563178d2727cdddcfddd
jc.fernandez <jcfdezrguez@gmail.com>
2022-04-26 23:09:52 +0200

    SlideShow 1

--- wikifilms/src/app/components/components.module.ts
+++ wikifilms/src/app/components/components.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    SlideshowComponent
   ],
   //Exportación necesaria para poder importar este componente desde el resto de páginas que harán uso de esta barra
   exports: [
