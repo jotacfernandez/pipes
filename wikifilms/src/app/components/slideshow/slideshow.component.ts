2aaf46f76e052b14925d5ce3e146b476920b1bc5
jc.fernandez <jcfdezrguez@gmail.com>
2022-04-27 12:13:46 +0200

    Imagen Poster y texto Slideshow

--- wikifilms/src/app/components/slideshow/slideshow.component.ts
+++ wikifilms/src/app/components/slideshow/slideshow.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Film } from 'src/app/interfaces/PosterResult';
+import Swiper, {Navigation} from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit{
 
   //Para recibir datos del componente padre (home) a nuestro Slideshow tenemos que definirlo en inputs
   @Input() films: Film[]=[];
-
+  
   constructor() { }
+
+  //Inicializacion
+  
+
+  ngAfterViewInit(): void {
+    const slideSwiper = new Swiper('.swiper', {
+      
+      // Optional parameters
+      loop: true,
+       // Navigation arrows
+      });
+  }
+
+  
+  
 
   ngOnInit(): void {
     console.log(this.films);
   }
-
 }
