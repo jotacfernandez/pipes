99e77421c5647270ff41bd681a724b2d10bf3051
jc.fernandez <jcfdezrguez@gmail.com>
2022-04-27 22:57:31 +0200

    Scroll infinito 1

--- wikifilms/src/app/pages/home/home.component.ts
+++ wikifilms/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Film } from 'src/app/interfaces/PosterResult';
 import { FilmsService } from 'src/app/services/films.service';
 
@@ -8,16 +8,36 @@ import { FilmsService } from 'src/app/services/films.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
+  public filmsCarousel: Film[] = [];
   public films: Film[] = [];
+  
+
+  //Decorador para controlar en que momento llegamos al final de la página haciendo scroll
+  //Esto será necesario para generar otra a continuación consiguiendo el efecto de scroll infinito
+  @HostListener('window:scroll', ['$event'])
+  onscroll(){
+    const posicion_scroll = document.documentElement.scrollTop;
+    const max_scroll = document.documentElement.scrollHeight;
+    
+    if (posicion_scroll+990 >= max_scroll) {
+      //console.log('pagina Superada')
+      this.filmsService.getPoster().subscribe( res => {
+        //Con push iremos añadiendo cada nueva página de resultados para que se vaya mostrando de manera "infinita"
+        this.films.push(...res.results);
+      });
+    }
+  }
 
 //Para inyectar el servicio hacemos uso del constructor
   constructor( private filmsService: FilmsService ) { }
+
   ngOnInit(): void {
     this.filmsService.getPoster()
       .subscribe(res => {
-       // console.log(res.results);
-        this.films = res.results;
+       // Obtenemos los dos arrays, uno para el grid de carátulas y el otro para el slide o carousel
+       this.films = res.results;
+       this.filmsCarousel = res.results;
       })
   }
 }
--- wikifilms/src/app/services/films.service.ts
+++ wikifilms/src/app/services/films.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { PosterResultado } from '../interfaces/PosterResult';
   
 
@@ -12,11 +12,25 @@ import { PosterResultado } from '../interfaces/PosterResult';
 })
 export class FilmsService {
 
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private pagina= 1;
   constructor( private http: HttpClient) { }
 
-  getPoster():Observable<PosterResultado> {
-    return this.http.get<PosterResultado>('https://api.themoviedb.org/3/movie/now_playing?api_key=99cd84992241f8b27486288ae4bfe53c&language=es-ES&page=1');
+  get params(){
+    return {
+      api_key: '99cd84992241f8b27486288ae4bfe53c',
+      language: 'es-ES',
+      page: this.pagina.toString()
+    }
   }
 
-
+  getPoster():Observable<PosterResultado> {
+    return this.http.get<PosterResultado>(`${this.baseUrl}/movie/now_playing`, {
+      params: this.params
+    }).pipe(
+      tap(() =>{
+        this.pagina++;
+      })
+    )
+  }
 }
